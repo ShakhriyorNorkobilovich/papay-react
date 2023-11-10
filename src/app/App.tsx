da2005074e18ba84b915ff1b9e2c25e07e044c76
ShakhriyorNorkobilovich <shaxroyorbek2101@gmail.com>
2023-11-11 01:59:02 +0900

    feat: develop routing based on papay project

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,14 @@
+import { Box, Button, Container, Stack, Typography } from '@mui/material'
 import React from 'react';
 import '../css/App.css';
-import { Box, Button, Container, Stack, Typography } from '@mui/material';
-import { RippleBadge } from './MaterialTheme/styled';
-import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Switch, Route, Link,} from "react-router-dom";
-import Users from './components/users';
-import Dishes from './components/dishes';
+import { RestaurantPage } from './screens/RestaurantPage';
+import { CommunityPage } from './screens/CommunityPage';
+import { OrdersPage } from './screens/OrdersPage';
+import { MemberPage } from './screens/MembersPage';
+import { HelpPage } from './screens/HelpPage';
+import { LoginPage } from './screens/LoginPage';
+import { HomePage } from './screens/HomePage';
 
 function App() {
   return (
@@ -14,13 +17,25 @@ function App() {
       <nav>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/restaurant">RestaurantPage</Link>
           </li>
           <li>
-            <Link to="/Dishes">Dishes</Link>
+            <Link to="/community">CommunityPage</Link>
           </li>
           <li>
-            <Link to="/users">Users</Link>
+            <Link to="/orders">OrdersPage</Link>
+          </li>
+          <li>
+            <Link to="/member-page">MemberPage</Link>
+          </li>
+          <li>
+            <Link to="/help">HelpPage</Link>
+          </li>
+          <li>
+            <Link to="/login">LoginPage</Link>
+          </li>
+          <li>
+            <Link to="/">HomePage</Link>
           </li>
         </ul>
       </nav>
@@ -28,16 +43,28 @@ function App() {
       {/* A <Switch> looks through its children <Route>s and
           renders the first one that matches the current URL. */}
       <Switch>
-        <Route path="/Dishes">
-          <Dishes />
+        <Route path="/restaurant">
+          <RestaurantPage />
         </Route>
-        <Route path="/users">
-          <Users />
+        <Route path="/community">
+          <CommunityPage />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <MemberPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+          </Route>
+        <Route path="/login">
+          <LoginPage />
         </Route>
         <Route path="/">
-          <Container>
-            <Home />
-          </Container>
+          
+            <HomePage />
+          
         </Route>
       </Switch>
     </div>
